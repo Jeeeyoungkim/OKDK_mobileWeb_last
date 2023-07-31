d67d15b314f434a23d2684106a48e7964be1490d
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-07-31 18:20:49 +0900

    커피컴포넌트, ListBox 컴포넌트

---
+++ okdk-web-prj/src/components/CoffeeComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+
+const CoffeeBackground = styled.div`
+  height: 3.4rem;
+  display: flex;
+  justify-content: center;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 50%;
+  background-color: ${(props) => props.color || "white"};
+`;
+
+const CoffeeComponentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 2.5rem;
+  height: 3.4rem;
+  object-fit: cover;
+  padding: 0.5rem;
+`;
+
+const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 0.5rem;
+`;
+const Description = styled.p`
+  color: #000;
+  text-align: center;
+  font-size: 0.75rem;
+  font-weight: 600;
+  margin: 0;
+  margin-top: 0;
+  padding: 0;
+`;
+
+const CoffeeComponent = ({
+  imgURI,
+  first_description,
+  second_description,
+  background,
+}) => {
+  return (
+    <CoffeeComponentContainer>
+      <CoffeeBackground color={background}>
+        <Image src={imgURI} alt="Image" />
+      </CoffeeBackground>
+
+      <DescriptionContainer>
+        <Description>{first_description}</Description>
+        <Description>{second_description}</Description>
+      </DescriptionContainer>
+    </CoffeeComponentContainer>
+  );
+};
+
+export default CoffeeComponent;
---
+++ okdk-web-prj/src/components/ListBox.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+const ListBoxContainer = styled.div`
+  width: 20rem;
+  height: 9rem;
+  border-radius: 1.25rem;
+  background: linear-gradient(180deg, #fff 0%, rgba(255, 255, 255, 0.25) 100%);
+  border-radius: 1.25rem;
+  border: 1px solid #96b3d9;
+  box-shadow: 0px 4px 4px 0px #96b3d9;
+  // filter: blur(5px);
+`;
+
+const TitleText = styled.p`
+  margin: 0;
+  color: #000;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const TitleContainer = styled.div`
+  margin: 1.19rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ListBox = ({ listTitle }) => {
+  return (
+    <ListBoxContainer>
+      <TitleContainer>
+        <TitleText>{listTitle}</TitleText>
+        <div>더보기</div>
+      </TitleContainer>
+    </ListBoxContainer>
+  );
+};
+
+export default ListBox;
