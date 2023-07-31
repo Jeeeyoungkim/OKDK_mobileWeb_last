5257b64fed97022c48ad7c586ef0c00eeffb4f52
seokhyun Yoon <gah1234@naver.com>
2023-07-31 21:24:59 +0900

    Morecards 페이지 생성
    Morecards 페이지 생성

--- okdk-web-prj/src/pages/CardSetting.js
+++ okdk-web-prj/src/pages/CardSetting.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Modal from '../components/Modal';
 
 export default function CardSetting() {
     return (
         <div>
-            
+            <Modal/>
         </div>
     );
 }
