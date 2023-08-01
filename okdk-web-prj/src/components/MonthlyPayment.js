965ed19e5f47117cf3d851e47b123b8dacbe8766
kimyeonchul <yeonchul0807@nate.com>
2023-08-02 04:52:33 +0900

    Feat: Init MonthlyPayment components

--- okdk-web-prj/src/components/MonthlyPayment.js
+++ okdk-web-prj/src/components/MonthlyPayment.js
@@ -1,21 +1,5 @@
 import React from "react";
-import styled from "styled-components";
 
-export const Container = styled.div`
-  width: 100%;
-`;
-
-export default function MonthlyPayment({ monthlypayment }) {
-  const fill = "rgb(5, 108, 242)";
-  var average = 0;
-  monthlypayment.map((item, index) => {
-    average += item.month_payment;
-  });
-  var data = [average / 3];
-  var labels = ["평균"];
-  monthlypayment.map((item, index) => {
-    data.push(item.month_payment);
-    labels.push(item.month);
-  });
-  return <Container></Container>;
+export default function MonthlyPayment() {
+  return <div></div>;
 }
