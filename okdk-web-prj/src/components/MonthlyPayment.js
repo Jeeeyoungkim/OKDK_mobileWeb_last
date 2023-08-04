a317b29fe0171c96c5aba22e4e679f124095505f
kimyeonchul <yeonchul0807@nate.com>
2023-08-04 21:47:39 +0900

    Feat: Add monthlyPayment graph

--- okdk-web-prj/src/components/MonthlyPayment.js
+++ okdk-web-prj/src/components/MonthlyPayment.js
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Bar, getElementAtEvent } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-export default function MonthlyPayment() {
-  return <div></div>;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export default function MonthlyPayment({ labels, data }) {
+  const chartRef = useRef();
+
+  const avgLabels = ["평균", ...labels.map((month) => `${Number(month)}월`)];
+  const avgData = [
+    data.reduce((acc, value) => acc + value, 0) / data.length,
+    ...data,
+  ];
+
+  const backgroundColors = ["#F29B0C", "#056CF2", "#056CF2", "#056CF2"];
+  const borderColors = ["#F29B0C", "#056CF2", "#056CF2", "#056CF2"];
+  const chartData = {
+    labels: avgLabels,
+    datasets: [
+      {
+        data: avgData,
+        backgroundColor: backgroundColors,
+        borderColor: borderColors,
+        borderWidth: 1,
+      },
+    ],
+  };
+  const onClick = (event) => {
+    const activeElement = getElementAtEvent(chartRef.current, event);
+    if (activeElement.length > 0) {
+      const dataIndex = activeElement[0].index;
+      const clickedLabel = chartData.labels[dataIndex];
+      console.log("Clicked Label:", clickedLabel);
+    }
+  };
+
+  const chartOptions = {
+    scales: {
+      y: {
+        display: false, // y축 표시하지 않음
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+  return (
+    <Bar
+      ref={chartRef}
+      data={chartData}
+      options={chartOptions}
+      onClick={onClick}
+    />
+  );
 }
