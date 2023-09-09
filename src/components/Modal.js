423c7c131cf750b8988d5730b6f61664e433f191
kimyeonchul <yeonchul0807@nate.com>
2023-09-09 16:08:49 +0900

    test

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import BasicButton from "./Button";
 import ArrowBack from "../assets/images/arrowBack.svg";
+import { useNavigate } from "react-router-dom";
 
 const Overlay = styled.section`
   z-index: 1;
