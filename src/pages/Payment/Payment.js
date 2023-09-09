155a555fd0f95495b1d546c7cfacded6e1327a36
kimyeonchul <yeonchul0807@nate.com>
2023-09-10 02:09:41 +0900

    Update Payment.js

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -202,7 +202,7 @@ export default function Payment() {
           </ListBox>
         )}
 
-        {/* <ListBox listTitle={"월별 결제 내역"}>
+        <ListBox listTitle={"월별 결제 내역"}>
           {threeCurrentMonths.length > 0 ? (
             <MonthlyPayment
               navigation={navigation}
@@ -214,7 +214,7 @@ export default function Payment() {
           ) : (
             <UndefinedText>월별 결제 내역이 없어요.</UndefinedText>
           )}
-        </ListBox> */}
+        </ListBox>
       </ScrollWrap>
     </Body>
   );
