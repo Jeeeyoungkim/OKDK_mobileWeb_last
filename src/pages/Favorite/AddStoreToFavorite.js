f3c6e676b9831e9a48a5d7a0d5d953c95b8b46ff
kimyeonchul <yeonchul0807@nate.com>
2023-09-09 15:33:21 +0900

    SearchInput 없애기

--- src/pages/Favorite/AddStoreToFavorite.js
+++ src/pages/Favorite/AddStoreToFavorite.js
@@ -77,12 +77,6 @@ export default function AddStoreToEarning() {
         buttonDisable={selectedStore === null}
         basicButtonOnClick={() => handleButtonClick()}
       >
-        {/* <SearchInputWrap>
-          <SearchInput />
-          <SearchButton>
-            <FiSearch size={"1.25rem"} />
-          </SearchButton>
-        </SearchInputWrap> */}
         <BrandComponentWrap>
           {nonFavoriteBrandData.map((data, index) => (
             <BrandComponent key={index} onClick={() => handleSelect(data.id)}>
